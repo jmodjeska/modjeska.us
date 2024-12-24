1d7fe7e0f018422fb4b07d55978c7a7df2f48a44
Jeremy Modjeska <jeremy@modjeska.us>
2024-12-23 21:09:54 -0800

    Brute force rewrite handling

--- build/copy_wp_style_slugs.rb
+++ build/copy_wp_style_slugs.rb
@@ -33,8 +33,33 @@ puts '-=> Build step: copy files to support WP-style slugs'
   Dir.each_child("#{build_dir}/#{dir}") do |file|
     next if file == 'index.html'
     next unless File.extname(file) == '.html'
+
+    # pictures-snow-day.html
     slug = "#{dir}-#{file}"
-    puts "-=> Copying #{build_dir}/#{dir}/#{file} to #{build_dir}/#{slug}"
-    FileUtils.cp("#{build_dir}/#{dir}/#{file}", "#{build_dir}/#{slug}")
+
+    # snow-day
+    basename = File.basename(file, File.extname(file))
+
+    # /build/site/pictures/snow-day.html
+    source = "#{build_dir}/#{dir}/#{file}"
+
+    # /build/site/pictures-snow-day.html
+    root_level_file = "#{build_dir}/#{slug}"
+
+    # /build/site/pictures-snow-day
+    slug_equivalent_dir = "#{build_dir}/#{dir}-#{basename}"
+
+    # /build/site/pictures-snow-day/index.html
+    index_file = "#{build_dir}/#{dir}-#{basename}/index.html"
+
+    # Create root-level HTML pages: /pictures-snow-day.html
+    puts "-=> Copying #{source} to #{root_level_file}"
+    FileUtils.cp(source, root_level_file)
+
+    # Create root-level directories: /pictures-snow-day/index.html
+    # This handles /pictures-snow-day/ with the trailing slash
+    puts "-=> Copying #{source} to #{index_file}"
+    FileUtils.mkdir_p(slug_equivalent_dir)
+    FileUtils.cp(source, index_file)
   end
 end
